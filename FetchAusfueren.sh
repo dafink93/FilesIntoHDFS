63a1d751a1e3b62c634bc4b5b456d20792a5c50b
vitorkrasniqi <47925295+vitorkrasniqi@users.noreply.github.com>
2019-04-18 20:37:55 +0200

    Add files via upload

---
+++ FetchAusfueren.sh
@@ -0,0 +1,5 @@
+ssh  bd01@cl-hpelb1-50-gw-01-lx-ub16.lxd 'wget "https://raw.githubusercontent.com/vitorkrasniqi/FilesIntoHDFS/master/GetDatafromStockmarket.sh"'
+
+ssh  bd01@cl-hpelb1-50-gw-01-lx-ub16.lxd 'sh GetDatafromStockmarket.sh'
+
+
