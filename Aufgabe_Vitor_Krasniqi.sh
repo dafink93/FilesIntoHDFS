208a34682e0b7534a1c1d63cd23834fda726be4a
vitorkrasniqi <47925295+vitorkrasniqi@users.noreply.github.com>
2019-04-18 16:36:07 +0200

    Add files via upload

---
+++ Aufgabe_Vitor_Krasniqi.sh
@@ -0,0 +1,29 @@
+
+#Verbinde dich mit deinem Cluster
+
+#ssh bd01@cl-hpelb1-50-gw-01-lx-ub16.lxd
+
+mkdir Senden
+
+cd Senden
+
+for i in AAPL  AMZN  CLDR  CSCO  DBX  FB  GOOGL  HPQ  IBM  JNPR  MSFT  NFLX  NTAP  ORCL  RHT  SNAP  SPOT  TSLA  TWTR  VMW
+
+do
+     wget "https://api.unibit.ai/historicalstockprice/$i?range=20y&interval=100&AccessKey=demo"
+     wget "https://api.unibit.ai/companyprofile/$i?AccessKey=demo"
+     wget "https://api.unibit.ai/companyprofile/$i?datatype=csv&AccessKey=demo"
+     wget "https://api.unibit.ai/historicalstockprice/$i?range=20y&interval=10&datatype=csv&AccessKey=demo"
+done
+
+
+cd ..
+
+hadoop fs -mkdir AufgabeFetch
+
+
+
+hadoop fs -put Senden/ AufgabeFetch
+
+
+hadoop fs -ls AufgabeFetch/Senden
