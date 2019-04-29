7b09e0824c940c8f98d38d2dfd6907484db421e0
vitorkrasniqi <47925295+vitorkrasniqi@users.noreply.github.com>
2019-04-29 17:25:30 +0200

    Add files via upload

---
+++ runAufgabeFetch.sh
@@ -0,0 +1,34 @@
+#Verbinde dich mit deinem Cluster
+
+#ssh bd01@cl-h.....
+
+mkdir Senden
+
+cd Senden
+
+for i in AAPL  AMZN  
+
+do
+#Historicalstockprice in Json
+     wget "https://api.unibit.ai/historicalstockprice/$i?range=1y&interval=100&AccessKey=demo"
+#Historicalstockprice in CSV
+     wget "https://api.unibit.ai/historicalstockprice/$i?range=1y&interval=10&datatype=csv&AccessKey=demo"
+#Companyprofile in Json
+     wget "https://api.unibit.ai/companyprofile/$i?AccessKey=demo"
+#Companyprofile in CSV
+     wget "https://api.unibit.ai/companyprofile/$i?datatype=csv&AccessKey=demo"
+
+done
+
+
+cd ..
+
+hadoop fs -mkdir AufgabeFetch
+
+
+
+hadoop fs -put Senden/ AufgabeFetch
+
+
+hadoop fs -ls AufgabeFetch/Senden
+
